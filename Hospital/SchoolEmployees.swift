169e9239f414a75e8f76f2799b4da253b9f9d98c
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-10-20 15:22:49 -0400

    Completed

--- Hospital/SchoolEmployees.swift
+++ Hospital/SchoolEmployees.swift
@@ -4,12 +4,26 @@
 
 
 
+struct AdvisoryBoardMember : Payable, TimeOff {
+    var name: String
+    var vacationDays: Int = 30
+    func wages() -> Double {
+        return 100_000.00
+    }
+    
+}
 
+struct Principal : Payable, TimeOff, Reprimand {
+    var name: String
+    var vacationDays: Int = 20
+    func wages() -> Double {
+        return 80_000.00
+    }
+}
 
+struct Teacher : Payable, TimeOff, Teach {
+    var name: String
+    var vacationDays: Int = 15
+}
 
-
-
-
-
-
-
+protocol Employee : Payable, TimeOff { }
